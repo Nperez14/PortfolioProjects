6dba5d910b388f6e763aeb3161f5437efdf9ddaa
Nperez14 <100253244+Nperez14@users.noreply.github.com>
2023-01-27 17:36:51 -0500

    Add files via upload

---
+++ MainGame.java
@@ -0,0 +1,10 @@
+package com.example.maingame;
+
+import com.example.gameoutline.GameOutline;
+
+public class MainGame {
+    public static void main(String[] args) {
+
+        new GameOutline();
+    }
+}
